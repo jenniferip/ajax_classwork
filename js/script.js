3853a4d0ec06b3689122e9f26b702e8ab0918837
Jennifer Penuelas <penuelas.jen@gmail.com>
2018-02-03 14:41:15 -0800

    Implement New York Times' article search API for addresses of interest

--- js/script.js
+++ js/script.js
@@ -22,6 +22,20 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+ address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    // NYT Implementation
+    var nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='
+        + cityStr + '&sort=newest&api-key=2179bc2f0b834359939303e5bba0da3f';
+    $.getJSON(nytURL, function (data) {
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' + '<a href="'
+                + article.web_url + '">' + article.headline.main + '</a>'
+                + '<p>' + article.snippet + '</p>' + '</li>');
+        };
+    })
+
     return false;
 }
 
